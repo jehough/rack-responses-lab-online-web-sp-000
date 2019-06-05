916e2864ee83a64de99ea5c0c51925c2b4f3985f
Jim Hough <teachlife@outlook.com>
2019-06-05 00:37:00 +0000

    Automatically backed up by Learn

---
+++ app/application.rb
@@ -0,0 +1,10 @@
+class Application
+  def call(env)
+    resp = Rack::Response.new
+    hour = Time.now.strftime('%H').to_i
+    if hour >= 12
+      resp.write "Good Afternoon!"
+    else
+      resp.write "Good Morning!"
+    end
+  end
